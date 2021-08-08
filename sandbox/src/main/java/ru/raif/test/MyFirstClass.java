a800982e0768cf0e96c6f66ba6c19931b2187506
artem-a-vlasov <87501238+artem-a-vlasov@users.noreply.github.com>
2021-08-08 22:13:45 +0300

    Update MyFirstClass.java

--- sandbox/src/main/java/ru/raif/test/MyFirstClass.java
+++ sandbox/src/main/java/ru/raif/test/MyFirstClass.java
@@ -2,6 +2,8 @@ package ru.raif.test;
 
 public class MyFirstClass {
 
-    public static void main(String[] args) {System.out.println("Hello World!");}
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 
 }
