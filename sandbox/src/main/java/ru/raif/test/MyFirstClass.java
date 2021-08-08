0b7ac6370df42c4e258c35709fd07bbb645269dd
artem-a-vlasov <87501238+artem-a-vlasov@users.noreply.github.com>
2021-08-09 00:54:45 +0300

    Вычисление с помощью метода

--- sandbox/src/main/java/ru/raif/test/MyFirstClass.java
+++ sandbox/src/main/java/ru/raif/test/MyFirstClass.java
@@ -6,16 +6,8 @@ import static java.lang.Math.sqrt;
 public class MyFirstClass {
 
     public static void main(String[] args) {
-        Point p1 = new Point();
-        p1.x = 2;
-        p1.y = 5;
-        Point p2 = new Point();
-        p2.x = 4;
-        p2.y = 7;
-        System.out.println("Расстояние между двумя точками = " + distance(p1,p2));
-    }
-
-    public static double distance(Point p1, Point p2){
-        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+        Point p1 = new Point(2,5);
+        Point p2 = new Point(4,6);
+        System.out.println("Расстояние между двумя точками = " + (p1.distance((p2))));
     }
 }
